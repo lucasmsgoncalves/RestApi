b1b78cb88bf75424c945b1a74502952d2c443e1c
Lucas <goncalves.lucasmatheus@gmail.com>
2019-04-22 22:10:42 -0300

    Conexão com o banco de dados

---
+++ server-auth/app/actions/sabores.ts
@@ -0,0 +1,38 @@
+import { Get} from '../decorators';
+import {Action} from '../kernel/action';
+import {ActionType} from '../kernel/route-types';
+import { KernelUtils } from '../kernel/kernel-utils';
+import {MySQLFactory} from '../mysql/mysql_factory';
+
+export class SaboresAction extends Action {
+
+    private generateSQL(tamanho : number) : string {
+        return 'select sabores.sabor, st.idSabor ' + 
+               'from sabores_has_tamanhos as st ' +
+               'left join sabores on sabores.id = st.idSabor ' +
+               'where st.idTamanho ='+ tamanho;
+    }
+
+    @Get('/sabores/:tamanho')
+    public getSabores(){
+        let tamanho = this.req.params.tamanho;
+
+        new KernelUtils().createExceptionApiError('1002', 'Tamanho da pizza não informado', (tamanho == null || tamanho == undefined));
+        new KernelUtils().createExceptionApiError('1003', 'Tamanho da pizza inválido', (tamanho < 1 || tamanho > 5));
+
+        new MySQLFactory().getConnection().select(this.generateSQL(tamanho)).subscribe(
+            (data : any) => {
+                console.log("data",data);
+                this.sendAnswer(data);
+            },
+            (error : any) => {
+                console.log("ERRO",error);
+                this.sendError(error);
+            }
+        );
+    }
+
+    defineVisibility() {
+        this.actionEscope = ActionType.atPublic;
+    }
+}
---
+++ server-auth/app/actions/tamanhos.ts
@@ -0,0 +1,30 @@
+import {Get} from '../decorators';
+import {Action} from '../kernel/action';
+import {ActionType} from '../kernel/route-types';
+import {MySQLFactory} from '../mysql/mysql_factory';
+import {KernelUtils} from '../kernel/kernel-utils';
+import {VPUtils} from '../utils/vputils';
+
+export class TamanhosAction extends Action {
+
+    private generateSQL() : string {
+        return 'select id, name from tamanhos';
+    }
+
+    @Get('/tamanhos')
+    public getTamanhos(){
+        new MySQLFactory().getConnection().select(this.generateSQL()).subscribe(
+            (data : any) => {
+                this.sendAnswer(data);
+            },
+            (error : any) => {
+                this.sendError(error);
+
+            }
+        );
+    }
+
+    defineVisibility() {
+        this.actionEscope = ActionType.atPublic;
+    }
+}
